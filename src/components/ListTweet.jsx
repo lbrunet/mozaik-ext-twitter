216ac50cd3478f37688dba40ef95d24eb64b1c43
Laurent Brunet <lbrunet@jolicode.com>
2015-12-12 13:57:43 +0100

    Fix comments review

--- src/components/ListTweet.jsx
+++ src/components/ListTweet.jsx
@@ -1,31 +1,28 @@
 var React  = require('react');
-var Media  = require('./Media');
+var TweetMedia  = require('./TweetMedia');
 var moment = require('moment');
 
 var ListTweet = React.createClass({
-    render() {
-        var cssClasses = 'list__item';
-        var entities   = this.props.tweet.entities;
+  render() {
+    var cssClasses = 'list__item';
+    var entities   = this.props.tweet.entities;
+    var tweetMedia;
 
-        if (entities.hasOwnProperty('media')) {
-            var tweetMedia = entities.media.map(media => {
-                return <Media key={media.id} media={media} />
-            });
-        }
-
-        return (
-            <div className={cssClasses}>
-                {this.props.tweet.text}
-                <div>
-                    <i className="fa fa-retweet" /> {this.props.tweet.retweet_count}&nbsp;
-                    <i className="fa fa-heart" /> {this.props.tweet.favorite_count}
-                </div>
-                <div>
-                    {tweetMedia}
-                </div>
-            </div>
-        );
+    if (entities.hasOwnProperty('media') && entities.media.length > 0) {
+      tweetMedia = entities.media.map(media => <TweetMedia key={media.id} media={media} />);
     }
+
+    return (
+      <div className={cssClasses}>
+        {this.props.tweet.text}
+        <div>
+          <i className="fa fa-retweet" /> {this.props.tweet.retweet_count}&nbsp;
+          <i className="fa fa-heart" /> {this.props.tweet.favorite_count}
+        </div>
+        {tweetMedia}
+      </div>
+    );
+  }
 });
 
 module.exports = ListTweet;
